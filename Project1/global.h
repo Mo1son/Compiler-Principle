1e5d327985f4e1b8736caf2cd9c605506783e79c
Mo1son <152183499+Mo1son@users.noreply.github.com>
2025-01-04 18:27:08 +0800

    Add files via upload

---
+++ Project1/global.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef GOBALS_H_INCLUDED
+#define GOBALS_H_INCLUDED
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif // GOBALS_H_INCLUDED
